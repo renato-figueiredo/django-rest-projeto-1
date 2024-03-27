0d4161ca1bc1012f10680f4070101a089a99a2cd
Renato Figueiredo <renatofigueiredoalves@gmail.com>
2024-03-27 16:12:52 -0300

    add model aluno and curso

--- escola/models.py
+++ escola/models.py
@@ -1,3 +1,35 @@
 from django.db import models
 
-# Create your models here.
+class Aluno(models.Model):
+    nome = models.CharField(max_length=255)
+    rg = models.CharField(max_length=9)
+    cpf = models.CharField(max_length=11)
+    data_nascimento = models.DateField()
+
+    def __str__(self):
+        """
+        Retorna uma representação de string do objeto.
+
+        :return: A representação de string do objeto, que é o valor do atributo 'nome'.
+        :rtype: str
+        """
+        return self.nome
+
+class Curso(models.Model):
+    NIVEL = (
+        ('B', 'Básico'),
+        ('I', 'Intermediário'),
+        ('A', 'Avançado'),
+    )
+    codigo_curso = models.CharField(max_length=10)
+    descricao = models.CharField(max_length=100)
+    nivel = models.CharField(max_length=1, choices=NIVEL, blank=False, null=False, default='B')
+
+    def __str__(self):
+        """
+        Um método para retornar a descrição do objeto como uma string.
+
+        :return: A representação de string do objeto, que é o valor do atributo 'nome'.
+        :rtype: str
+        """
+        return self.descricao
--- setup/settings.py
+++ setup/settings.py
@@ -38,7 +38,9 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
 
-    'rest-framework',
+    'escola',
+
+    'rest_framework',
 ]
 
 MIDDLEWARE = [
