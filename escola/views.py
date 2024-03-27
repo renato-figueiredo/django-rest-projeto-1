d8af7f787a79bf84a53f6555d05d0e819d2c0d66
Renato Figueiredo <renatofigueiredoalves@gmail.com>
2024-03-27 20:07:26 -0300

    update views to use viewstes

--- escola/views.py
+++ escola/views.py
@@ -1,9 +1,17 @@
-from django.http import JsonResponse
+from rest_framework import viewsets
+from escola.models import Aluno, Curso
+from escola.serializer import AlunoSerializer, CursoSerializer
 
-def alunos(request):
-    if request.method == 'GET':
-        aluno = {
-            'id': 1,
-            'nome': 'Renato',
-        }
-        return JsonResponse(aluno)
+class AlunosViewSet(viewsets.ModelViewSet):
+    """
+    Exibe todos alunos
+    """
+    queryset = Aluno.objects.all()
+    serializer_class = AlunoSerializer
+
+class CursosViewSet(viewsets.ModelViewSet):
+    """
+    Exibe todos os cursos
+    """
+    queryset = Curso.objects.all()
+    serializer_class = CursoSerializer
